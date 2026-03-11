4a168d2319e642c0d571289a77ba52d42c974c34
sebastianmel <smeliot86@gmail.com>
2026-03-11 20:49:34 +0100

    add comments & finish project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import TodoItem from "./TodoItem";
+import { Construction } from "lucide-react";
 
 type Priority = "Urgente" | "Moyenne" | "Basse" 
 
@@ -35,6 +36,7 @@ function App() {
     setPriority("Moyenne")
     console.log(newTodos)
   }
+  // function deleteTodo for delete a todo by id
   function deleteTodo(id: number) {
     const newTodos = todos.filter(todo => todo.id !== id)
     setTodos(newTodos)
@@ -46,12 +48,33 @@ function App() {
   } else {
     filteredTodos = todos.filter(todo => todo.priority === filter)
   }
+  // count of each priority
   const urgenteCount = todos.filter(todo => todo.priority === "Urgente").length
   const moyenneCount = todos.filter(todo => todo.priority === "Moyenne").length
   const basseCount = todos.filter(todo => todo.priority === "Basse").length
   const totalCount = todos.length
-
-
+const [selectedTodos , setSelectedTodos] = useState<Set<number>>(new Set())
+// function toggleSelectTodo for select or deselect a todo
+function toggleSelectTodo(id: number) {
+  const newSelectedTodos = new Set(selectedTodos)
+  if (newSelectedTodos.has(id)){
+    newSelectedTodos.delete(id)
+  } else {
+    newSelectedTodos.add(id)
+  }
+  setSelectedTodos(newSelectedTodos)
+} 
+// function finishSelected for delete all selected todos
+function finishSelected() {
+  const newTodos = todos.filter((todo) =>{if (selectedTodos.has(todo.id)){
+    return false
+  }else {    
+    return true
+  }
+})
+  setTodos(newTodos)
+  setSelectedTodos(new Set())
+}
   return (
    <div className="flex justify-center">
       <div className="w-2/3 flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
@@ -79,36 +102,51 @@ function App() {
         </div>
         <div className="space-y-2 flex-1 h-fit my-5">
 
-          <div className="flex flex-wrap gap-4">
-            <button className={`btn btn-soft ${filter === "Tous" ? "btn-primary" : ""}`}
-            onClick={() => setFilter("Tous")}
-            >Tous({totalCount})
+        <div className="flex item-center justyfy-between">
+            <div className="flex flex-wrap gap-4">
+              <button className={`btn btn-soft ${filter === "Tous" ? "btn-primary" : ""}`}
+              onClick={() => setFilter("Tous")}
+              >Tous({totalCount})
+              </button>
+              <button className={`btn btn-soft ${filter === "Urgente" ? "btn-primary" : ""}`}
+              onClick={() => setFilter("Urgente")}
+              >Urgente({urgenteCount})
+              </button>
+              <button className={`btn btn-soft ${filter === "Moyenne" ? "btn-primary" : ""}`}
+              onClick={() => setFilter("Moyenne")}
+              >Moyenne({moyenneCount})
+              </button>
+              <button className={`btn btn-soft ${filter === "Basse" ? "btn-primary" : ""}`}
+              onClick={() => setFilter("Basse")}
+              >Basse({basseCount})
+              </button>
+            </div>
+            <button className="btn btn-primary"
+            disabled={selectedTodos.size === 0}
+            onClick={finishSelected}>
+              Finir toutes les tâches({selectedTodos.size})
             </button>
-            <button className={`btn btn-soft ${filter === "Urgente" ? "btn-primary" : ""}`}
-            onClick={() => setFilter("Urgente")}
-            >Urgente({urgenteCount})
-            </button>
-            <button className={`btn btn-soft ${filter === "Moyenne" ? "btn-primary" : ""}`}
-            onClick={() => setFilter("Moyenne")}
-            >Moyenne({moyenneCount})
-            </button>
-            <button className={`btn btn-soft ${filter === "Basse" ? "btn-primary" : ""}`}
-            onClick={() => setFilter("Basse")}
-            >Basse({basseCount})
-            </button>
-          </div>
+        </div>
+    
 
           {filteredTodos.length > 0 ? (
             <ul className="divide-y divide-primary/20%">
               {filteredTodos.map(todo =>(
                 <li key={todo.id}>
-                  <TodoItem todo={todo}/>
+                  <TodoItem todo={todo}
+                  isSelected={selectedTodos.has(todo.id)}
+                  onDelete={() => deleteTodo(todo.id)}
+                  onToggleSelect={() => toggleSelectTodo(todo.id)}
+                  />
                 </li>
               ))}
             </ul>
             ):(
 
-            <div>vide</div>
+            <div className="flex justify-center items-center flex-col p-5">
+              <Construction strokeWidth={1} className="w20 h20 text-primary"/>
+              <p className="text-sm">Aucune tâche à afficher</p>
+            </div>
             
             )}
         </div>
--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -9,25 +9,27 @@ type Todo = {
 }
 type Props ={
     todo: Todo
+    onDelete: () => void
+    isSelected?: boolean
+    onToggleSelect?: (id: number) => void
 }
-const TodoItem = ({todo}: Props) =>{
-    function deleteTodo(id: number): void {
-        console.log(`Deleting todo with id: ${id}`);
-        
-    }
+const TodoItem = ({todo, onDelete ,isSelected, onToggleSelect}: Props) =>{
 
     return (
         <li className="p-3">
             <div className="flex justify-between items-center">
                 <div className="flex items-center gap-2">
-                    <input type="checkbox" className="checkbox checkbox-primary checkbox-sm" />
+                    <input type="checkbox" className="checkbox checkbox-primary checkbox-sm"
+                    checked={isSelected}
+                    onChange={() => onToggleSelect && onToggleSelect(todo.id)}
+                    />
                     <span className="text-md font-bold">{todo.text}</span>
                     <span  className={`badge badge-soft ${todo.priority === "Urgente" ? "badge-error" : todo.priority === "Moyenne" ? "badge-warning" : "badge-success"}`}>
                         {todo.priority}
                     </span>
                 </div>
-                <button className="btn btn-sm btn-error btn-soft">
-                    <Trash className="w-4 h-4" onClick={() => deleteTodo(todo.id)}/>
+                <button className="btn btn-sm btn-error btn-soft" onClick={onDelete}>
+                    <Trash className="w-4 h-4" />
                 </button>
             </div>
         </li>
